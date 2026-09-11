b0f87022354ab06637701aae8a4831455beb0fa3
Yakov <jacobrybalko@gmail.com>
2026-09-11 14:56:24 +0300

    Добавьте файлы проекта.

---
+++ PluginExtension.cs
@@ -0,0 +1,22 @@
+﻿using Autodesk.AutoCAD.Runtime;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoCAD_2027_Plugin_CS1
+{
+    public class PluginExtension : IExtensionApplication
+    {
+        public void Initialize()
+        {
+            // Add your initialization code here
+        }
+
+        public void Terminate()
+        {
+            // Add your termination code here
+        }
+    }
+}
